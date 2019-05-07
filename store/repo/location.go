a9d9482d07c2bb8f98973e2f7b219d8d13d6e460
Ahsun Ahmed <ahmed.ahsun@gmail.com>
2019-05-07 16:48:53 +0600

    add location repository

---
+++ store/repo/location.go
@@ -0,0 +1,19 @@
+package repo
+
+import "github.com/rls/ping-api/store/model"
+import "github.com/rls/ping-api/svc/cache"
+
+// Location ...
+type Location struct {
+	cacheSvc cache.ICacheService
+}
+
+// Save ...
+func (l *Location) Save(key string, locations ...*model.Location) error {
+	return l.cacheSvc.GeoAdd(key, locations...)
+}
+
+// NewLocation returns a new location repo
+func NewLocation(cacheSvc cache.ICacheService) ILocation {
+	return &Location{cacheSvc}
+}
